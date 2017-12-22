b65c8814f08098366f4a2d3b8f1840381e605a2d
aureliebc <34154812+aureliebc@users.noreply.github.com>
2017-12-22 09:29:28 +0100

    Add files via upload

---
+++ Liaison_Com_OF/FormulaireLiaison.php
@@ -0,0 +1,11 @@
+<?php $this->load->helper('url'); ?>
+
+<!Doctype>
+<html>
+
+	<head>
+		<meta charset="utf-8" />
+    	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>css/test.css">
+		<title> BDD Client </title>
+	</head>
+</html>
